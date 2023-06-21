60e7a575692840c0bf15330ff0ee730413399f76
seanarch <zhuqingyun@outlook.com>
2023-06-21 11:52:18 -0600

    Login/register and cart icons

--- src/App.js
+++ src/App.js
@@ -21,6 +21,26 @@ function App() {
               <i className="fa fa-search" />
             </button>
           </div>
+          <div className="login-container p-0">
+            <i className="fa fa-user-circle user-icon" />
+            <h5>
+              <a href="#">Login</a>
+            </h5>
+            /
+            <h5>
+              <a href="#">Register</a>
+            </h5>
+          </div>
+          <div className="cart-wishlist">
+            <ul>
+              <li className="list-icon">
+                <i className="fa fa-heart"></i>
+              </li>
+              <li className="list-icon">
+                <i className="fa fa-shopping-cart"></i>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
