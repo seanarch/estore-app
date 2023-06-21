cee63ed0f1b74ef1ad6ea10429138b4dae0c369d
seanarch <zhuqingyun@outlook.com>
2023-06-21 11:41:51 -0600

    Add dropdown menu

--- src/App.js
+++ src/App.js
@@ -9,6 +9,13 @@ function App() {
             <h1>eStore</h1>
           </div>
           <div className="inp-container p-0 my-4 w-50 h-25 bg-white">
+            <div className="dropdown m-0 p-0">
+              <select className="select-btn w-100 p-0 m-0" name="" id="">
+                <option value="">Men</option>
+                <option value="">Women</option>
+                <option value="">Kids</option>
+              </select>
+            </div>
             <input className="form-control" placeholder="Search" />
             <button>
               <i className="fa fa-search" />
