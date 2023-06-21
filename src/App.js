2f2224ab2cba4215c7d9ad1dbfc43d7fb81a64b4
seanarch <zhuqingyun@outlook.com>
2023-06-21 11:33:39 -0600

    Header component with css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header bg-dark">
+        <div className="row">
+          <div className="brand my-1">
+            <h1>eStore</h1>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
