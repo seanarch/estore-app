6213ca80d5e921b9c39b923875b246d6dc237aa0
seanarch <zhuqingyun@outlook.com>
2023-06-21 11:38:54 -0600

    Add search box

--- src/App.js
+++ src/App.js
@@ -8,6 +8,12 @@ function App() {
           <div className="brand my-1">
             <h1>eStore</h1>
           </div>
+          <div className="inp-container p-0 my-4 w-50 h-25 bg-white">
+            <input className="form-control" placeholder="Search" />
+            <button>
+              <i className="fa fa-search" />
+            </button>
+          </div>
         </div>
       </div>
     </div>
