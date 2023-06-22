9bb594637af5e8d4e28fda5e845b11366970b1a4
seanarch <zhuqingyun@outlook.com>
2023-06-22 15:50:28 -0600

    Add cat nav and scss

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import TopNav from "./Components/TopNav";
+import CatNav from "./Components/CatNav";
 
 function App() {
   return (
     <div className="App">
       <TopNav />
+      <CatNav />
     </div>
   );
 }
